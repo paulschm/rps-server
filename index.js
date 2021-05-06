c0742e66f660fbba4a61fd040311c91ff9ba53ef
martingasser <martin.gasser@gmail.com>
2021-05-06 07:16:49 +0200

    working client-server communication

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 const express = require('express')
 const app = express()
-const ws = require('ws')
 const cors = require('cors')
+const SocketIO = require('socket.io')
 
-const corsOptions = {
-    origin: 'http://localhost:8080'
-}
+// we don't need the REST functionality right now
+// as communication will be based on socket.io only in the first steps
+// later we will add a game statistics REST API
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// const corsOptions = {
+//     origin: 'http://localhost:8080'
+// }
 
-const wsServer = new ws.Server({ noServer: true });
+// app.use(cors(corsOptions))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
-app.use((req, res, next) => {
-    res.wss = wsServer
-    next()
-})
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to Rock-Paper-Scissors API'
-    })
-})
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Welcome to Rock-Paper-Scissors API'
+//     })
+// })
 
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT || 8081
-
-wsServer.on('connection', socket => {
-    socket.on('message', message => console.log(message));
+const io = SocketIO(http, {
+    cors: {}
 })
+
+io.on('connection', (socket) => {
+    console.log('got a connection')
+
+    socket.on('message', (arg) => {
+        console.log('Got a message:', arg)
+        setTimeout(() => {
+            socket.emit('serverMessage', 'Hello from server')
+        }, 2000)
+    })
+
+})
+
+const PORT = process.env.PORT || 8081
 
 const server = http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`)
-})
-
-server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, socket => {
-        wsServer.emit('connection', socket, request)
-    })
 })
